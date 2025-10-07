e79d51998d9410173a6c4c489873bcc63cb44e06
JUANA WESLEY <26109981@edgehill.ac.uk>
2025-10-07 15:23:33 +0100

    ASCII art <3

---
+++ week1_lab.py
@@ -0,0 +1,27 @@
+print("Hello CIS1702")
+
+# Storing text (a string)
+student_name = "Alex"
+# Storing a whole number (an integer)
+student_age = 20
+# Print the variables to the console
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+num1 = 7
+num2 = 2
+
+answer = num1 + num2
+print("answer:", answer)
+
+# ascii art with text inside
+
+name = "Juana"
+username = "Jingles"
+location = "Fairy Island"
+
+print("+---------------------------+")
+print("|  Name: " + name + "              |")
+print("|  Username: " + username + "        |")
+print("|  Location: " + location + "   |")
+print("+---------------------------+")
